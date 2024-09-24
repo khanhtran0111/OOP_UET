6ea562a005670ee6198467bf3727248a354a5e2b
Tran Gia Khanh <90601002+khanhtran0111@users.noreply.github.com>
2024-09-24 13:48:18 +0700

    Week4

---
+++ Week4/Student.java
@@ -0,0 +1,69 @@
+public class Student {
+    private String name;
+
+    private String id;
+
+    private String group;
+
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    Student() {
+        name = "Student";
+        id = "000";
+        group = "K62CB";
+        email = "uet@vnu.edu.vn";
+    }
+
+    Student(String name, String id, String email) {
+        this.name = name;
+        this.id = id;
+        group = "K62CB";
+        this.email = email;
+    }
+
+    Student(Student s) {
+        this.name = s.name;
+        this.id = s.id;
+        this.group = s.group;
+        this.email = s.email;
+    }
+
+    String getInfo() {
+        return name + " - " + id + " - " + group + " - " + email;
+    }
+
+    public static void main(String[] args) {
+    }
+}
